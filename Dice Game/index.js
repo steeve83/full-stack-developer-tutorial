e253eb9ba32d0c12bd7770d6536f21aab434e2f9
Steeve Pierre <brightlightdeveloper@gmail.com>
2025-12-16 22:15:21 -0500

    completed project

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,9 +1,9 @@
 let randomNumber1 = Math.floor(Math.random() * 6)+ 1;//number 1-6
 let randomDiceImage = ("dice") + randomNumber1 + ".png";// dice1.png to dice6.png
-let randomImageSource = "images/" + randomDiceImage;
+let randomImageSource = "images/" + randomDiceImage;    
 let image1 =  document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
-
+// Second Dice but changed 
 let randomNumber2 = Math.floor(Math.random() * 6)+ 1;//number 1-6
 let randomImageSource2 = "images/dice" + randomNumber2 + ".png";// use "images/dice" as a string and just add randomNumber2 to simplify it.
 let image2 = document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
